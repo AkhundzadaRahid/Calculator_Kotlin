8f1bcb903da428fdfbd1e5e8815e832aaa59ac0f
Rahid Axundzada <axundzada@gmail.com>
2020-03-06 11:00:53 +0400

    11.00

--- app/src/main/java/com/example/calc2/MainActivity.kt
+++ app/src/main/java/com/example/calc2/MainActivity.kt
@@ -11,26 +11,97 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
-
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    var dott: Boolean = true
+    var operaa: Boolean = false
+
+    var a: Double = 0.0
+    var b: Double = 0.0
+    var i:Int=0
+    var j:Int=0
+
+
+    var numArray = emptyArray<String>()
+    var oppArray = emptyArray<String>()
+    var num=0
+    var ops=0
+
+    val opp= arrayOf("+","-","/","*")
+
+    fun dot(view: View) {
+        if (dott == true) {
+            DisplayWrite.append((view as Button).text)
+            dott = false
+        }
+    }
+
+    fun writeNum(view: View) {
+
+        operaa = true
+
+        if (DisplayWrite.text == "+") {
+            DisplayWrite.text = ""
+           // Display.append((view as Button).text)
+        } else {
+            DisplayWrite.append((view as Button).text)
+        }
 
     }
 
-
-        fun writeNum(view: View){
-            Display.append((view as Button).text)
-        }
-
-        fun Clear(view: View){
+        fun Clear(view: View) {
+            DisplayWrite.text = ""
             Display.text=""
-    }
-
-        fun opera(view: View){
-                Display.append((view  as Button).text)
+            operaa=false
+            dott=true
         }
 
+        fun opera(view: View) {
 
-}
+            dott = true
+            if (operaa == true) {
+                DisplayWrite.append(" ")
+                DisplayWrite.append((view as Button).text)
+                DisplayWrite.append(" ")
+                operaa = false
+            }
+        }
+
+        fun Beraber(view: View) {
+
+            val input = DisplayWrite.text
+            val SplitInput=input.split(' ')
+            val j=SplitInput.size
+
+           /* for (i in 0..j){
+                if (i % 2 == 1){
+                    numArray.set(SplitInput[i])
+
+                }
+                else if(i % 2 == 0){
+                    var oppArray =SplitInput[i]
+                }
+            }*/
+
+
+
+            while (i==j){
+                i++
+                if (i % 2 == 1){
+                    SplitInput[i]
+                }
+                else if(i % 2 == 0){
+                    SplitInput[i]
+                }
+           }
+
+
+
+}}
+
+
+
+
